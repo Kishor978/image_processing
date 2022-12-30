97d19eae55c37df5a78790d26aa73a4612160fb7
kishor <thaunnakishor@gmail.com>
2022-12-30 20:57:11 +0545

    Reading video

---
+++ video_read.py
@@ -0,0 +1,16 @@
+import cv2
+video = cv2.VideoCapture("new/MERO_YO_MAAN_LE_AaJA.mp4")
+print("video", video)
+while True:
+    ret, frame = video.read()
+    frame=cv2.resize(frame,(700,450))
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow("frame", frame)
+    cv2.imshow("gray frame",gray)
+    k=cv2.waitKey(25)& 0xFF         #mask=0xFF
+    if k==ord("q"):
+        break
+video.relese()
+cv2.destroyAllWindows()
+
+
