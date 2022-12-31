75f91d360933060d268ae0e6f5cdfd78c804df56
kishor <thaunnakishor@gmail.com>
2022-12-31 16:14:17 +0545

    added comments

--- mini_projects/screen_recoder.py
+++ mini_projects/screen_recoder.py
@@ -6,19 +6,19 @@ import numpy as np
 rs=p.size()
 fn=input("Enter file name and path to store: ")
 fps=60.0        #frame rate
-fourcc=cv2.VideoWriter_fourcc(*"XVID")
-output=cv2.VideoWriter(fn,fourcc,fps,rs)
+fourcc=cv2.VideoWriter_fourcc(*"XVID")  #video ofrmat
+output=cv2.VideoWriter(fn,fourcc,fps,rs) #path,codec,frame persec,screenresolution
 
 #creating recording module
 cv2.namedWindow("Recording",cv2.WINDOW_NORMAL)
-cv2.resizeWindow("Recording",(640,460))
+cv2.resizeWindow("Recording",(640,460))         #resizing frame
 while True:
-    img=p.screenshot()
-    f=np.array(img)
+    img=p.screenshot()      #capture frames
+    f=np.array(img)         #creating numpy array for captured frame
     f=cv2.cvtColor(f,cv2.COLOR_BGR2RGB)
-    output.write(f)
+    output.write(f)         #writing frame in file
     # cv2.imshow("recording",f)
-    if cv2.waitKey(1)== ord("q"):
-        break
+    if cv2.waitKey(1)== ord("q"):   # press 'q' to quit
+        break                       
 output.release()
 cv2.destroyAllWindows()
